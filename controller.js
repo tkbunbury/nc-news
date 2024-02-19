dd23fc6acb26329ebda397003d7676c038e01683
Takai <tkbunbury@gmail.com>
2024-02-19 15:21:50 +0000

    Implemented endpoint to return object containing all available endpoints to client

--- controller.js
+++ controller.js
@@ -1,17 +1,28 @@
 const {
     selectTopics,
+    readEndpointsFile,
 
 } = require(`${__dirname}/model.js`)
+
 
 async function getTopics (req, res, next) {
     try{
         const topics = await selectTopics();
         res.status(200).send({ topics })
-
     }
     catch (err) {
         next(err)
     }
 }
 
-module.exports = { getTopics };
+async function getEndpoints (req, res, next) {
+    try{
+        const endpoints = await readEndpointsFile();
+        res.status(200).send( endpoints )
+    }
+    catch (err) {
+        next(err)
+    }
+}
+
+module.exports = { getTopics, getEndpoints };
