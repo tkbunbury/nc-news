395d1a04ba8a619e72ec3168b0d0bd06b322d200
Takai <tkbunbury@gmail.com>
2024-02-20 15:27:29 +0000

    Implemented endpoint to return array of article objects in descending order to client

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const {
     getTopics,
     getEndpoints,
     getArticleById,
+    getArticles,
 
 } = require(`${__dirname}/controller.js`)
 
@@ -12,6 +13,8 @@ app.get('/api/topics', getTopics)
 app.get('/api', getEndpoints)
 
 app.get('/api/articles/:article_id', getArticleById)
+
+app.get('/api/articles', getArticles)
 
 app.use((err, req, res, next) => {
     if (err.code === "22P02") {
--- model.js
+++ model.js
@@ -1,5 +1,6 @@
 const db = require('./db/connection');
 const fs = require('fs/promises');
+const articles = require('./db/data/test-data/articles');
 
 
 selectTopics = async () => {
@@ -28,4 +29,14 @@ selectArticleById = async (article_id) => {
     return articleWithString;
 }
 
-module.exports = { selectTopics, readEndpointsFile, selectArticleById };
+selectArticles = async () => {
+
+    const result = await db.query('SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(comments.body) AS INTEGER) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url ;')
+    
+    const articles = result.rows;
+    const sortedArticles = [...articles];
+    sortedArticles.sort((a,b) => new Date(b.created_at) - new Date(a.created_at));
+    return sortedArticles;
+}
+
+module.exports = { selectTopics, readEndpointsFile, selectArticleById, selectArticles };
